412c0f944460961cc00d0f2357ba54c498c753fd
dlentz92 <dlentz92@gmail.com>
2020-01-30 15:39:39 -0500

    connecting mongoDB

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-require ("./services/passport");
 const authRoutes = require("./routes/authRoutes")
-
+const mongoose = require("mongoose")
 const app = express();
 
+require("./services/passport");
 require("./routes/authRoutes")(app);
+require("./models/user")
+
+mongoose.connect(keys.mongoURI)
+const keys = require("./config/keys")
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
