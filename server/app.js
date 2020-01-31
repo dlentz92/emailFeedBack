e760d6d6bd9fabc71ae82d4104a3dd547e8044ad
dlentz92 <dlentz92@gmail.com>
2020-01-31 12:34:51 -0500

    connect passport/user/atlas-not working

--- server/app.js
+++ server/app.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const authRoutes = require("./routes/authRoutes")
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+ 
+mongoose.connect(keys.mongoURI);
+ 
 const app = express();
-
-require("./services/passport");
-require("./routes/authRoutes")(app);
-require("./models/user")
-
-mongoose.connect(keys.mongoURI)
-const keys = require("./config/keys")
-
-
+ 
+require('./routes/authRoutes')(app);
+ 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
+app.listen(PORT);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose")
 const keys = require('../config/keys');
+
+const User = mongoose.model("users");
 
 passport.use(new GoogleStrategy(
     {
@@ -9,10 +12,11 @@ passport.use(new GoogleStrategy(
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        console.log("access token", accessToken);
-        console.log("refreshToken", refreshToken);
-        console.log("profile", profile);
-        console.log("done", done)
+        new User ({googleId: profile.id}).save();
+        // console.log("access token", accessToken);
+        // console.log("refreshToken", refreshToken);
+        // console.log("profile", profile);
+        // console.log("done", done)
 
     }
-));
+)); 
