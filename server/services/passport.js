9b95295a15b3890165faf9f4e0939e1cc7799b02
dlentz92 <dlentz92@gmail.com>
2020-02-06 13:35:40 -0500

    mongoDB connected and working correctly

--- server/services/passport.js
+++ server/services/passport.js
@@ -12,10 +12,24 @@ passport.use(new GoogleStrategy(
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id }).save();
-        // console.log("access token", accessToken);
-        // console.log("refreshToken", refreshToken);
-        // console.log("profile", profile);
-        // console.log("done", done)
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    // ALREADY HAVE A RECORD WITH EXISTING PROFILE ID
+                    // DO NOT MAKE NEW RECORD
+                    done(null, existingUser)
+                } else {
+                    // MAKE A NEW RECORD
+                    new User({ googleId: profile.id })
+                    .save()
+                    .then(user> done(null, user))
+                }
+            })
+
+
+        console.log("access token", accessToken);
+        console.log("refreshToken", refreshToken);
+        console.log("profile", profile);
+        console.log("done", done)
     }
 )); 
