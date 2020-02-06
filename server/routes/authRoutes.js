7422619034888ada6ec797729fb48c5a946e6a23
dlentz92 <dlentz92@gmail.com>
2020-02-06 15:30:42 -0500

    authentication complete

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,4 +9,8 @@ passport.authenticate('google', {
 );
 
 app.get('/auth/google/callback', passport.authenticate('google') )
+
+app.get("/api/current_user", (req, res) => {
+  res.send(req.user);
+});
 };
