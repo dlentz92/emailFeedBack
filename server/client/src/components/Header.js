f1bc38e234e10cd68cefc7fe3e0ad0b6656b2419
dlentz92 <dlentz92@gmail.com>
2020-02-17 20:13:53 -0500

    nav link added. not functioning

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,11 +1,22 @@
-import React, { Component } from "react";
-
+import React, {Component} from "react";
 
 class Header extends Component {
-    render() {
+    render () {
         return (
             <div>
-            <h1>hello</h1>
+            <Nav variant="pills" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
             </div>
         );
     }
