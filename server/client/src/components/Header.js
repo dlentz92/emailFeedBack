b2472302dd827e4bddc5ad77bc469a267576b317
dlentz92 <dlentz92@gmail.com>
2020-02-18 12:51:53 -0500

    bootstrap partially working. additional pkgs dwnld

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,23 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { Fragment } from 'react';
+import {
+    Navbar, NavbarBrand, Nav, NavItem, NavLink
+} from 'reactstrap';
 
 class Header extends Component {
-    render () {
+    render() {
         return (
-            <div>
-            <Nav variant="pills" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link href="/home">Active</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="disabled" disabled>
-      Disabled
-    </Nav.Link>
-  </Nav.Item>
-</Nav>
-            </div>
+            <Fragment>
+                <Navbar color="faded" light expand="md">
+                    <NavbarBrand href="/">
+                        Email Feedback
+                </NavbarBrand>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem className="d-flex align-items-center">
+                            <NavLink className="font-weight-bold" href="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem className="d-flex align-items-center">
+                            <NavLink className="font-weight-bold" href="https://www.techiediaries.com/react-bootstrap">
+                                Login with Google
+                    </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+            </Fragment>
         );
     }
 }
--- server/client/src/setupProxy.js
+++ server/client/src/setupProxy.js
@@ -1,5 +1,12 @@
 const proxy = require('http-proxy-middleware')
- 
-module.exports = function(app) {
-    app.use(proxy(['/api', '/auth/google'], { target: 'http://localhost:5000' }));
+
+module.exports = function (app) {
+    app.use(proxy
+        (
+            ['/api', '/auth/google'],
+            { target: 'http://localhost:5000' },
+            ["/api/*"],
+            { "target": "http://localhost:5000" }
+        )
+    );
 }
