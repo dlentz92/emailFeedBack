3f35062937356b9f097242693280e6d5af14ae8b
dlentz92 <dlentz92@gmail.com>
2020-02-17 20:09:12 -0500

    bootstrap added-not successful

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from "./Header"
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveryNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
-    return(
+    return (
         <div>
-        Hi There
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveryNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,10 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from 'redux';
 
 import App from "./components/App"
-import reducers from "./reducers";
+import "./reducers";
 
 const store = createStore(() => [], {}, applyMiddleware())
 
