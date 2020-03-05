7f4395e8f359df84f0619be622f89240441ee455
dlentz92 <dlentz92@gmail.com>
2020-03-05 14:45:58 -0500

    payments initiated and header updated

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
+import Payments from "./Payments"
 import { Fragment } from 'react';
 
 
@@ -13,7 +14,10 @@ class Header extends Component {
       case false:
         return <li><a href="/auth/google">Login With Google</a></li>
       default:
-        return <li><a>Logout</a></li>
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
   render() {
